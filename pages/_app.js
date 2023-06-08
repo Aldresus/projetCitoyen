373e588adc8081fac0d125b39b4a2d98845f1171
hugo <hugochampy@gmail.com>
2023-06-08 17:46:22 +0200

    uniformisation cagnotte

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 import "style/globals.css";
 import CesiSVG from "@/assets/cesiSVG";
 import { Roboto } from "next/font/google";
+import { nFormatter } from "@/pages/index";
+import { useEffect, useState } from "react";
+import Emphasis from "@/components/primitives/emphasis";
 const font = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin-ext"],
 });
 const MyApp = ({ Component, pageProps }) => {
+  let [balance, setBalance] = useState("0");
+
+  useEffect(() => {
+    fetch("/api/balance")
+      .then((res) => res.json())
+      .then((data) => {
+        setBalance(nFormatter(data));
+      });
+  }, []);
   return (
     <div id="_app" className={font.className}>
       <Component {...pageProps} />
       <div className="fixed bottom-4 right-6 w-20 h-20">
         <CesiSVG />
+      </div>
+      <div className="fixed bottom-4 left-6 glassmorphism p-2">
+        <h1 className="text-xl">
+          <Emphasis>{balance}€ récoltés</Emphasis>
+        </h1>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import CTA from "@/components/cta";
 import PresentationProjetCitoyen from "@/components/presentationProjetCitoyen";
 import ExplicationDispositif from "@/components/explicationDispositif";
-import { useEffect, useState } from "react";
-import Emphasis from "@/components/primitives/emphasis";
+
 export function nFormatter(num) {
   if (!num) return "0";
   const lookup = [
@@ -27,15 +26,6 @@ export function nFormatter(num) {
 }
 
 export default function Home() {
-  let [balance, setBalance] = useState("0");
-
-  useEffect(() => {
-    fetch("/api/balance")
-      .then((res) => res.json())
-      .then((data) => {
-        setBalance(nFormatter(data));
-      });
-  }, []);
   return (
     <div className="bg-gray-100 overflow-y-hidden">
       <div className=" h-screen w-full lg:snap-y lg:snap-mandatory overflow-y-scroll">
@@ -44,11 +34,6 @@ export default function Home() {
         <ExplicationDispositif />
         <div className="lg:hidden h-32"></div>
         {/*<Test></Test>*/}
-      </div>
-      <div className="fixed left-2 bottom-2 glassmorphism p-4">
-        <h1 className="text-xl">
-          <Emphasis>{balance}€ récoltés</Emphasis>
-        </h1>{" "}
       </div>
     </div>
   );
