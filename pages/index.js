ae5fda0841016409540815abf34229d10d7ea817
hugo <hugochampy@gmail.com>
2023-06-11 15:41:38 +0200

    changement timing toast

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,6 @@ export default function Home() {
             className="glassmorphism_red p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
             open={open}
             onOpenChange={setOpen}
-            duration={20000}
           >
             <Toast.Title className="text-red-800 font-bold">
               Paiement annulé
