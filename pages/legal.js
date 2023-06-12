12ee1d803d341c9fcc77f316be1815a879398096
hugo <hugochampy@gmail.com>
2023-06-12 09:59:27 +0200

    + de mentions légales

--- pages/legal.js
+++ pages/legal.js
@@ -24,6 +24,12 @@ export default function MentionsLegales() {
               Designed by Freepik
             </a>
           </p>
+          <p className="text-lg leading-relaxed m-4 text-gray-600">
+            Paiement sécurisé par Stripe, l&#39;argent est collecté par AX
+            INTERCAMPUS, association loi 1901 située au 2 Avenue Jean Monnet,
+            31520 Ramonville Saint-Agne avant d&#39;être reversé à Emmaüs
+            France.
+          </p>
         </div>
       </div>
     </div>
